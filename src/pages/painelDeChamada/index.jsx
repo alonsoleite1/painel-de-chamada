acb916c794329b89f2304e925f47e0beaf51ca36
alonsoleite1 <allonso17@gmail.com>
2025-05-05 15:12:10 -0300

    template

--- src/pages/painelDeChamada/index.jsx
+++ src/pages/painelDeChamada/index.jsx
@@ -4,8 +4,8 @@ const Painel = () => {
   return (
     <div className={styles.container}>
       <div className={styles.headerArea}>
-        <div className={styles.normal}><p>Atendimento Normal</p> <span> 01 </span></div>
-        <div className={styles.prioritario}><p>Atendimento Prioritário</p><span>02</span></div>
+        <div className={styles.normal}><p>ATENDIMENTO NORMAL</p> <span> 01 </span></div>
+        <div className={styles.prioritario}><p>ATENDIMENTO PRIORITÁRIO</p><span>02</span></div>
       </div>
       <div className={styles.slideArea}>
         {/* Aqui você pode usar um componente de slideshow, como react-slideshow-image */}
