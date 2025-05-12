4714e65692779a6ceca6b691bb9224b719483d21
alonsoleite1 <allonso17@gmail.com>
2025-05-12 19:57:15 -0300

    refatoração

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+import { useContext } from "react";
+import { UsuarioContext } from "./provider/userContext";
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+import { Spinner } from "react-loading-io";
 import RoutesMain from "./routes/routesMain";
 import "./styles/index.scss";
 
 function App() {
+  const { loading } = useContext(UsuarioContext);
+
+  const spinnerCfg = { left: "40%", transform: "translateY(150%)" };
   return (
     <>
-      <RoutesMain />
+        <ToastContainer
+          position="top-center"
+          autoClose={2000}
+        />
+        {loading ? <Spinner color="#3498db" style={spinnerCfg} /> : <RoutesMain />}
     </>
-  );
+  )
 }
 
 export default App;
